d86c9453f0f2954c81d2273f74aa08d82f410476
mateusz7812 <mateusz78122@gmail.com>
2020-02-24 12:04:39 +0100

    more tests

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -12,5 +12,12 @@ namespace UnitTestProject1
             var service = new WebApplication3.Services.Service(3);
             Assert.AreEqual(9, service.Calc());
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var service = new WebApplication3.Services.Service(0);
+            Assert.AreEqual(0, service.Calc());
+        }
     }
 }
